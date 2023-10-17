cc015d204de0f5b470938b4d7673aaa9bb0c8ba4
Martin Vu <martinvu@stud.ntnu.no>
2023-10-18 00:39:31 +0200

    Connect to GameRater db

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,10 +1,10 @@
-import { ApolloServer } from '@apollo/server';
-import { startStandaloneServer } from '@apollo/server/standalone';
-import { connect } from 'mongoose';
-import Review from '../models/review.js';
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+import { connect } from "mongoose";
+import Review from "../models/review.js";
 
-const MONGODB = 'mongodb://it2810-48.idi.ntnu.no:27017'
- 
+const MONGODB = "mongodb://it2810-48.idi.ntnu.no:27017/GameRater";
+
 const typeDefs = `#graphql
   type Review {
     _id: String
@@ -27,47 +27,53 @@ const typeDefs = `#graphql
   }
 
   type Mutation {
-    createReview(reviewInput: ReviewInput): String!
+    createReview(reviewInput: ReviewInput): Review!
     updateReview(ID: ID!, reviewInput: ReviewInput): String!
     deleteReview(ID: ID!): String!
   }
   `;
 
-  const resolvers = {
-    Query: {
-      async getReview(_, { ID }) {
-        const review = await Review.findById(ID);
-        return review;
-      },
-      async getReviews(_, { limit }) {
-        return Review.find().limit(limit);
-      }
-    }, 
-    Mutation: {
-      async createReview(_, { reviewInput : { author, title, content, rating }}) {
-        const res = await new Review({author, title, content, rating}).save();
-        return res._id;
-      },
-      async updateReview(_, { ID, reviewInput: { author, title, content, rating}}) {
-        await Review.updateOne({_id: ID}, { $set: {author, title, content, rating}});
-        return ID;
-      },
-      async deleteReview(_, { ID }) {
-        await Review.deleteOne({_id: ID});
-        return ID;
-      }
-    }
-  };
+const resolvers = {
+  Query: {
+    async getReview(_, { ID }) {
+      const review = await Review.findById(ID);
+      return review;
+    },
+    async getReviews(_, { limit }) {
+      return Review.find().limit(limit);
+    },
+  },
+  Mutation: {
+    async createReview(_, { reviewInput: { author, title, content, rating } }) {
+      const review = await new Review({ author, title, content, rating }).save();
+      return review;
+    },
+    async updateReview(
+      _,
+      { ID, reviewInput: { author, title, content, rating } }
+    ) {
+      await Review.updateOne(
+        { _id: ID },
+        { $set: { author, title, content, rating } }
+      );
+      return ID;
+    },
+    async deleteReview(_, { ID }) {
+      await Review.deleteOne({ _id: ID });
+      return ID;
+    },
+  },
+};
 
-  await connect(MONGODB);
+await connect(MONGODB);
 
-  const server = new ApolloServer({
-    typeDefs,
-    resolvers,
-  });
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
 
-  const { url } = await startStandaloneServer(server, {
-    listen: { port: 4000}
-  });
+const { url } = await startStandaloneServer(server, {
+  listen: { port: 4000 },
+});
 
-  console.log(`🚀 Server ready at ${url}`)
+console.log(`🚀 Server ready at ${url}`);
