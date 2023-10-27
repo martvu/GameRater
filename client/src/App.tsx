b368168c5fa6c818e645159b852d534d8d60bab6
jorgolsl <jorgolsl@stud.ntnu.no>
2023-10-27 11:00:46 +0200

    UUID for each user is put in their localstorage

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,7 +4,20 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import GameListPage from './pages/GameListPage';
 import GameDetailPage from './pages/GameDetailPage';
 import { ReviewForm } from './components/ReviewForm';
+import { v4 as uuidv4 } from 'uuid';
+import { useEffect } from 'react';
+
 function App() {
+  useEffect(() => {
+    // Check if the UUID is already in localStorage
+    let userID: string | null = localStorage.getItem('userID');
+    // If not, generate a new UUID and save it to localStorage
+    if (!userID) {
+      userID = uuidv4();
+      localStorage.setItem('userID', userID);
+    }
+  }, []);
+
   return (
     <>
       <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
