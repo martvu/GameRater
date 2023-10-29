2ce4b94ec06947fb09fb166cb0bcbb488dd37908
Martin Vu <martinvu@stud.ntnu.no>
2023-10-29 22:40:33 +0100

    Refactor button-variants name

--- client/src/components/ui/button.tsx
+++ client/src/components/ui/button.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { Slot } from '@radix-ui/react-slot';
 import { type VariantProps } from 'class-variance-authority';
-import { buttonVariants } from './buttonVariants';
+import { buttonVariants } from './button-variants';
 import { cn } from '@/lib/utils';
 
 export interface ButtonProps
