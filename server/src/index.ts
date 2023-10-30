d7876907276472fbfb88ce3ad3c8a2de0ef4d3b2
Martin Vu <martinvu@stud.ntnu.no>
2023-10-30 11:05:40 +0100

    Add resolver types to server with codegen

--- server/src/index.ts
+++ server/src/index.ts
@@ -5,82 +5,11 @@ import Review from "../models/review.js";
 import Game from "../models/game.js";
 import Genre from "../models/genre.js";
 import Platform from "../models/platform.js";
-import { read, readFileSync } from "fs";
+import { readFileSync } from "fs";
+import { resolvers } from "./resolvers.js";
 
 const MONGODB = "mongodb://it2810-48.idi.ntnu.no:27017/GameRater";
-
 const typeDefs = readFileSync("./src/schema.graphql", "utf8");
-
-const resolvers = {
-  Query: {
-    async getReview(_, { ID }) {
-      return await Review.findById(ID);
-    },
-    async getReviews(_, { limit }) {
-      return await Review.find().limit(limit);
-    },
-    async getGame(_, { ID }) {
-      return await Game.findById(ID);
-    },
-    async getGames(_, { limit, offset }) {
-      return await Game.find().skip(offset).limit(limit);
-    },
-    async getGenre(_, { id }) {
-      return await Genre.findOne({ id: id });
-    },
-    async getGenres(_, { limit }) {
-      return await Genre.find().limit(limit);
-    },
-    async getPlatform(_, { id }) {
-      return await Platform.findOne({ id: id });
-    },
-    async getPlatforms(_, { limit }) {
-      return await Platform.find().limit(limit);
-    },
-  },
-  Mutation: {
-    async createReview(_, { reviewInput: { author, title, content, rating, platform, gameID } }) {
-      const review = await new Review({
-        author,
-        title,
-        content,
-        rating,
-        platform,
-        gameID
-      }).save();
-      // Update the corresponding game's reviews array or create it if it doesn't exist
-      await Game.findByIdAndUpdate(gameID, {
-        $addToSet: { reviews: review._id },
-      });
-      return review;
-    },
-    async updateReview(
-      _,
-      { ID, reviewInput: { author, title, content, rating, platform, gameID } }
-    ) {
-      await Review.updateOne(
-        { _id: ID },
-        { $set: { author, title, content, rating, platform, gameID } }
-      );
-      return ID;
-    },
-    async deleteReview(_, { ID }) {
-      await Review.deleteOne({ _id: ID });
-      return ID;
-    },
-  },
-  Game: {
-    async genres(game) {
-      return await Genre.find({ id: { $in: game.genres } });
-    },
-    async platforms(game) {
-      return await Platform.find({ id: { $in: game.platforms } });
-    },
-    async reviews(game, { limit }) {
-      return await Review.find({ _id: { $in: game.reviews} }).limit(limit);
-    }
-  },
-};
 
 await connect(MONGODB);
 
---
+++ server/src/resolvers.ts
@@ -0,0 +1,85 @@
+import Game from '../models/game.js';
+import Genre from '../models/genre.js';
+import Platform from '../models/platform.js';
+import Review from '../models/review.js';
+import { Resolvers } from './__generated__/resolvers-types';
+
+export const resolvers: Resolvers = {
+  Query: {
+    getGame: async (_, { ID }) => {
+      return await Game.findById(ID);
+    },
+    getGames: async (_, { limit, offset }) => {
+      const games = await Game.find().skip(offset).limit(limit);
+      return games.map(game => game.toObject());
+    },
+    getReview: async (_, { ID }) => {
+      const review = await Review.findById(ID);
+        return review.toObject();
+    },
+    getReviews: async (_, { limit }) => {
+      const reviews = await Review.find().limit(limit);
+      return reviews.map(review => review.toObject());
+    },
+    getGenre: async (_, { id }) => {
+      const genre = await Genre.findOne( { id: id });
+      return genre.toObject();
+    },
+    
+    getGenres: async (_, { limit }) => {
+      const genres = await Genre.find().limit(limit);
+      return genres.map(genre => genre.toObject());
+    },
+    getPlatform: async (_, { id }) => {
+      const platform = await Platform.findOne( { id: id });
+      return platform.toObject();
+    },
+    getPlatforms: async (_, { limit }) => {
+      const platforms = await Platform.find().limit(limit);
+      return platforms.map(platform => platform.toObject());
+    },
+  },
+  Mutation: {
+    createReview: async(_, { reviewInput: { author, title, content, rating, platform, gameID } }) => {
+      const review = await new Review({
+        author,
+        title,
+        content,
+        rating,
+        platform,
+        gameID
+      }).save();
+      // Update the corresponding game's reviews array or create it if it doesn't exist
+      await Game.findByIdAndUpdate(gameID, {
+        $addToSet: { reviews: review._id },
+      });
+      return { ...review.toObject(), _id: review._id.toString() };
+    },
+    updateReview: async(
+      _,
+      { ID, reviewInput: { author, title, content, rating, platform, gameID } }
+    ) => {
+      await Review.updateOne(
+        { _id: ID },
+        { $set: { author, title, content, rating, platform, gameID } }
+      );
+      return ID;
+    },
+    deleteReview: async(_, { ID }) =>{
+      await Review.deleteOne({ _id: ID });
+      return ID;
+    },
+  },
+  Game: {
+    genres: async(game) =>{
+      return await Genre.find({ id: { $in: game.genres } });
+    },
+    platforms: async(game) => {
+      return await Platform.find({ id: { $in: game.platforms } });
+    },
+    reviews: async(game, { limit }: { limit: number }) => {
+      const reviews = await Review.find({ _id: { $in: game.reviews} }).limit(limit);
+      return reviews.map(review => ({ ...review.toObject(), _id: review._id.toString() }));
+    }
+  },
+};
