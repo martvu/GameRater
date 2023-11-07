df4b65a093e02b1652082c53b6a2206735e22e31
Martin Vu <martinvu@stud.ntnu.no>
2023-11-07 23:07:27 +0100

    Add tooltip to gamecard titles

--- client/src/components/GameCard.tsx
+++ client/src/components/GameCard.tsx
@@ -13,6 +13,12 @@ import { useQuery } from '@apollo/client';
 import { gql } from '../gql/';
 import Metascore from './Metascore';
 import FavoriteHeart from './FavoriteHeart';
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from './ui/tooltip';
 
 interface GameCardProps {
   game: Game;
@@ -44,22 +50,38 @@ export function GameCard({ game }: GameCardProps) {
     <Card className="relative h-[400px] min-w-[240px] max-w-[300px] overflow-hidden p-0">
       <CardHeader className="h-[320px] overflow-hidden">
         <div className="min-h-[320px] w-full duration-700 hover:scale-105">
-          {imageId && <img src={coverImageUrl} alt={name as string} className='object-cover' />}
+          {imageId && (
+            <img
+              src={coverImageUrl}
+              alt={name as string}
+              className="object-cover"
+            />
+          )}
         </div>
       </CardHeader>
       <CardContent className="relative px-3 text-start">
-        <Link to={`/game/${id}`} aria-label={`Link to ${name} detail page`}>
-          <CardTitle className="py-1 my-1 line-clamp-1 max-w-[240px] text-lg hover:opacity-50 duration-300 ">
-            {name}
-          </CardTitle>
-        </Link>
-        
-        
+        <TooltipProvider>
+          <Tooltip>
+            <TooltipTrigger asChild>
+              <Link
+                to={`/game/${id}`}
+                aria-label={`Link to ${name} detail page`}
+              >
+                <CardTitle className="my-1 line-clamp-1 max-w-[240px] py-1 text-lg duration-300 hover:opacity-50 ">
+                  {name}
+                </CardTitle>
+              </Link>
+            </TooltipTrigger>
+            <TooltipContent>
+              <p>{name}</p>
+            </TooltipContent>
+          </Tooltip>
+        </TooltipProvider>
       </CardContent>
       <CardFooter className="absolute bottom-0 left-0 mt-auto h-[40px] w-full px-3 pb-2">
         <div className="relative flex h-full w-full items-center">
           <div className="absolute left-0 flex items-center">
-            <Star className="mr-1 h-5 text-yellow-400 fill-yellow-400" />
+            <Star className="mr-1 h-5 fill-yellow-400 text-yellow-400" />
             <p>{rating === 0 ? '-' : rating}</p>
           </div>
           <div className="absolute right-10 flex items-center gap-2">
@@ -67,9 +89,9 @@ export function GameCard({ game }: GameCardProps) {
               metascore={aggregatedRating ? aggregatedRating : undefined}
             />
           </div>
-          <div className='absolute right-0'>
-          <FavoriteHeart game={game} />
-        </div>
+          <div className="absolute right-0">
+            <FavoriteHeart game={game} />
+          </div>
         </div>
       </CardFooter>
     </Card>
