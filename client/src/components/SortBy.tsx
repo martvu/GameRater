88e136ed86c65bd65dfbb79007b960f8dd42f459
jorgolsl <jorgolsl@stud.ntnu.no>
2023-11-07 14:05:34 +0100

    Added title sort z-a

--- client/src/components/SortBy.tsx
+++ client/src/components/SortBy.tsx
@@ -20,8 +20,11 @@ export default function SortBy() {
     if (value === 'Release Date Asc') {
       setSortBy({ field: 'first_release_date', order: 'asc' });
     }
-    if (value === 'Name') {
+    if (value === 'Name A-Z') {
       setSortBy({ field: 'name', order: 'asc' });
+    }
+    if (value === 'Name Z-A') {
+      setSortBy({ field: 'name', order: 'desc' });
     }
     if (value === 'Metascore') {
       setSortBy({ field: 'aggregated_rating', order: 'desc' });
@@ -47,7 +50,8 @@ export default function SortBy() {
           <SelectLabel>Sort By</SelectLabel>
           <SelectItem value="Release Date Desc">Release Date Desc</SelectItem>
           <SelectItem value="Release Date Asc">Release Date Asc</SelectItem>
-          <SelectItem value="Name">Name</SelectItem>
+          <SelectItem value="Name A-Z">Name A-Z</SelectItem>
+          <SelectItem value="Name Z-A">Name Z-A</SelectItem>
           <SelectItem value="Metascore">Metascore</SelectItem>
           <SelectItem value="User Rating">User Rating</SelectItem>
         </SelectGroup>
