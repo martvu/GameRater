d9c6a689aea2fc44dee020f6c720947d84aebb1a
Martin Vu <martinvu@stud.ntnu.no>
2023-11-16 23:35:28 +0100

    Config test setup and coverage

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,10 +1,18 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react-swc';
 import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    globals: true,
+    setupFiles: ["src/setupTests.ts"],
+    environment: "jsdom",
+    coverage: {
+      provider: "v8",
+    }
+  },
   base: '/project2',
   resolve: {
     alias: {
